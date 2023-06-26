696134a80fdacc25bf71ac0bd4e2c30214e14b72
Miller <millerredigolo@gmail.com>
2023-06-26 10:38:07 -0300

    moments.ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentComponent } from './components/pages/moment/moment.component';
+import { EditMomentComponent } from './components/pages/edit-moment/edit-moment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'moments/new',
+    component: NewMomentComponent,
+  },
+  {
+    path: 'moment/edit/:id',
+    component: EditMomentComponent,
+  },
+  {
+    path: 'moment/:id',
+    component: MomentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
